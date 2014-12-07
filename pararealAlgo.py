c73cd66142efde6ce991a0fd5653b7db6a28d2f5
Andy Shi <andyshi@college.harvard.edu>
2014-12-07 16:35:58 -0500

    it runs now

--- pararealAlgo.py
+++ pararealAlgo.py
@@ -10,7 +10,8 @@ from serial_euler import forward_euler
 from serial_euler import forward_euler_step
 
 # Parareal begin
-def startParareal(deriv, init, k, tmin, tmax, numSteps, qualityFactor = 100, comm):
+def startParareal(deriv, init, k, tmin, tmax, numSteps, comm, 
+        qualityFactor = 100):
   '''Starts running k iterations of the parareal algorithm.
   In each iteration, the program runs the coarse method in serial, then fine corrections in parallel
   The result is only gathered to the root process at every iteration'''
@@ -20,12 +21,9 @@ def startParareal(deriv, init, k, tmin, tmax, numSteps, qualityFactor = 100, com
 
   dt = (tmax-tmin) / (numSteps - 1)
 
-  # upast and unext vectors
-  upast = np.zeros(numSteps)
-  unext = np.zeros(numSteps)
-
   # all coarse
   upast = forward_euler(deriv, init, tmin, tmax, numSteps)
+  unext = None
 
   # if further iterations are required
   if k > 1:
@@ -47,27 +45,30 @@ def serial_coarse_with_correction(deriv, init, corrections, dt, tmin, tmax, numS
   '''Runs a coarse numerical method in serial, as part of the parareal algorithm.
   Will call the stepwise g_coarse function per step'''
 
-  unext = np.zeros(numSteps)
+  fixed_corrections = np.empty((0, init.shape[0]))
+  # corrections is a list
+  for arr in corrections:
+    fixed_corrections = np.concatenate((fixed_corrections, arr))
+  unext = np.zeros((numSteps+1, init.shape[0]))
 
   # initialize times
-  times = np.linspace(time_min, time_max, numSteps)[1:]
+  times = np.linspace(tmin, tmax, numSteps+1)
 
   # initial condition
   unext[0] = init
 
   # call forward euler per step and store in vector
-  for ind in xrange(numSteps-1):
+  for ind in xrange(numSteps):
     # get a coarse answer
     # TODO is this upast[ind] off by one?
-    unext[ind+1] = forward_euler_step(deriv, times[ind], unext[ind], dt)
+    unext[ind] = forward_euler_step(deriv, times[ind], unext[ind], dt)
 
     # update unext with corrections computed with upast
-    for ind in xrange(numSteps-1):
-      unext[ind+1] += unext[ind] + corrections[ind]
+    unext[ind+1] = unext[ind] + fixed_corrections[ind]
 
   return unext
 
-def parallel_corrections(upast, tmin, tmax, numSteps, qualityFactor, comm, p_root=0):
+def parallel_corrections(deriv, upast, tmin, tmax, numSteps, qualityFactor, comm, p_root=0):
   '''Parallel Portion of the Parareal Algorithm
   Divides the work based on the unext vector, into equal parameters
   Gathers the result on process p_root for the given iteration.
@@ -80,27 +81,30 @@ def parallel_corrections(upast, tmin, tmax, numSteps, qualityFactor, comm, p_roo
   numtasks = numSteps
 
   # values for the fine computations
-  fineNumSteps = numSteps*factor
+  dt = float(tmax - tmin) / (numSteps - 1)
+  fineNumSteps = numSteps*qualityFactor
   fineDt = dt / qualityFactor
 
   # broadcast upast
-  upast = comm.broadcast(upast, root=p_rot)
+  upast = comm.bcast(upast, root=p_root)
 
   # Start and end indices of the times to divide into with a +1 for python range offset
   start = getStart(numtasks, size, rank)
   end = getStart(numtasks, size, rank + 1)
 
   # initialize times
-  times = np.linspace(time_min, time_max, numSteps)[1:]
+  times = np.linspace(tmin, tmax, numSteps+1)
 
+  local_differences = np.empty((0, upast.shape[1]))
   # in a loop, compute fine and then corrections one piece at a time that processor is responsible for, serially
   while (start < end):
+    print("Rank: %d, start: %d" %(rank, start))
     fineResult = forward_euler(deriv, upast[start], times[start], times[start+1], fineNumSteps)
 
     # get difference for the last time step of the fine result and compare to coarse result answer for same time
-    difference = fineResult[-1] - upast[start+1]
+    difference = (fineResult[-1] - upast[start+1]).reshape(1, upast.shape[1])
     # concatenate the local results
-    local_differences = np.concatenate(local_unext, difference)
+    local_differences = np.concatenate((local_differences, difference), axis=0)
 
     # move to solve next step
     start += 1
@@ -126,6 +130,7 @@ if __name__ == '__main__':
   rank = comm.Get_rank()
 
   # Example specific parameters and derivatives with IC
+  lam = 1.
   def deriv(t, u):
     return lam * u
 
@@ -143,7 +148,8 @@ if __name__ == '__main__':
   p_start = MPI.Wtime()
 
   # Use parareal algorithm
-  p_result = startParareal(deriv, init, tmin, tmax, step, comm)
+  # we only want column 1 so numpy doesn't complain
+  p_result = startParareal(deriv, init, 2, tmin, tmax, numSteps, comm)
 
   # Communciation barrier and ending time with MPI timing function
   comm.barrier()
@@ -152,27 +158,33 @@ if __name__ == '__main__':
   # Check and output results on process 0
   if rank == 0:
 
+    p_result = p_result[:,0]
     # compute serial result
     s_start = time.time()
-    s_result = forward_euler(deriv, init, tmin, tmax, numSteps)
+
+    # we only want column 1 so numpy doesn't complain
+    s_result = forward_euler(deriv, init, tmin, tmax, numSteps)[:,0]
     s_stop = time.time()
 
     # compute exact result
-    e_result = np.exp(np.linspace(tmin, tmax, len(res)))
+    e_result = np.exp(np.linspace(tmin, tmax, numSteps+1))
 
     # compute errors
-    s_error = abs(s_result - e_result) / abs(e_result)
-    p_error = abs(p_result - e_result) / abs(e_result)
+    s_error = np.abs(s_result - e_result)# / abs(e_result)
+    p_error = np.abs(p_result - e_result)# / abs(e_result)
 
     # print timers
     print "Serial Time:   %f secs" % (s_stop - s_start)
     print "Parallel Time: %f secs" % (p_stop - p_start)
 
     # print results
-    print "Serial Result   = %f" % s_result
-    print "Parallel Result = %f" % p_result
-    print "Exact           = %f" % e_result
+    #print "Serial Result   = " + str(s_result)
+    print "Parallel Result = " + str(p_result)
+    #print "Exact           = " + str(e_result)
 
     # print errors
-    print "Serial Relative Error    = %e" % s_error
-    print "Parallel Relative Error  = %e" % p_error
+    print "Serial Relative Error    = %e" % np.max(s_error)
+    print "Parallel Relative Error  = %e" % np.max(p_error)
+
+    print "Parallel Error = " + str(p_error)
+    print "Serial Error = " + str(s_error)
--- serial_euler.py
+++ serial_euler.py
@@ -1,5 +1,8 @@
 import numpy as np
 
+def forward_euler_step(deriv, t, u, dt):
+    return u + dt * deriv(t, u)
+
 def forward_euler(deriv, init, time_min, time_max, numSteps):
     '''
     Solves a differential equation using Euler's method from time_min to
@@ -16,15 +19,16 @@ def forward_euler(deriv, init, time_min, time_max, numSteps):
     assert(init.shape == (dim,))
 
     # initialize times
-    times = np.linspace(time_min, time_max, numSteps)[1:]
+    times = np.linspace(time_min, time_max, numSteps+1)[1:]
+    time_step = float(time_max - time_min) / (numSteps - 1)
 
-    ys = np.zeros((nsteps, dim))
+    ys = np.zeros((numSteps+1, dim))
     ys[0] = init
 
     for (ind, t) in enumerate(times):
         time_ind = ind + 1
         curr = ys[time_ind-1]
-        ys[time_ind] = curr + time_step * deriv(t, curr)
+        ys[time_ind] = forward_euler_step(deriv, t, curr, time_step)
 
     return ys
 
@@ -35,10 +39,12 @@ if __name__ == "__main__":
     def deriv(t, u):
         return lam * u
 
+    nsteps = 200
     init = np.array([1.])
     
-    res = forward_euler(deriv, init, 0, 1, 0.01)[:,0]
-    
-    exact = np.exp(np.linspace(0,1,len(res)))
+    res = forward_euler(deriv, init, 0, 1, nsteps)[:,0]
     print res
+    print len(res)
+    
+    exact = np.exp(np.linspace(0,1,nsteps+1))
     print exact - res
